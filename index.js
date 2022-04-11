f71280e01881f8acaa1cd03026ea843a80ae9efe
Tejasvee Dighe <tejasveedighe@gmail.com>
2022-04-11 13:55:34 +0530

    some changes

--- index.js
+++ index.js
@@ -6,23 +6,23 @@ const articleRouter = require("./routes/articles");
 const methodOverride = require("method-override");
 
 mongoose.connect("mongodb://localhost/blog", {
-	useNewUrlParser: true,
-	useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
 });
 
 // used to override the method of the form
 app.use(methodOverride("_method"));
 
 // tells express to access the data in the request as object check in articles.js
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 
 // sets the html to view engine written in ejs
 app.set("view engine", "ejs");
 
 // simple route
 app.get("/", async (req, res) => {
-	const articles = await articleModel.find({}).sort({ createdAt: "desc" });
-	res.render("articles/index", { articles: articles });
+    const articles = await articleModel.find({}).sort({ createdAt: "desc" });
+    res.render("articles/index", { articles: articles });
 });
 
 app.use("/articles", articleRouter);
